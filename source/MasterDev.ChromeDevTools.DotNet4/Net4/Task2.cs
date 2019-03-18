a277962b1d9881ccc33ca4f7131ded02b35686d7
ststeiger <quandary82@hailmail.net>
2019-03-18 16:15:53 +0100

    Backport to .NET 4.0

---
+++ source/MasterDev.ChromeDevTools.DotNet4/Net4/Task2.cs
@@ -0,0 +1,118 @@
+﻿
+#define DOTNET4
+
+
+namespace System.Threading.Tasks
+{
+
+
+    public class Task2
+    {
+
+        public static System.Threading.Tasks.Task Run(System.Action action
+            , System.Threading.CancellationToken cancellationToken)
+        {
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(action, cancellationToken);
+#else
+            return System.Threading.Tasks.Task.Run(action, cancellationToken);
+#endif
+        }
+
+
+        public static System.Threading.Tasks.Task Run(System.Action action)
+        {
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(action);
+#else
+            return System.Threading.Tasks.Task.Run(action);
+#endif
+
+        }
+
+
+        public static System.Threading.Tasks.Task Run(System.Func<System.Threading.Tasks.Task> function)
+        {
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(function);
+#else
+            return System.Threading.Tasks.Task.Run(function);
+#endif
+
+        }
+
+
+        public static System.Threading.Tasks.Task Run(System.Func<System.Threading.Tasks.Task> function
+            , System.Threading.CancellationToken cancellationToken)
+        {
+
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(function, cancellationToken);
+#else
+            return System.Threading.Tasks.Task.Run(function, cancellationToken);
+#endif
+
+
+        }
+
+
+        public static System.Threading.Tasks.Task<TResult> Run<TResult>(System.Func<System.Threading.Tasks.Task<TResult>> function)
+        {
+
+
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(function);
+#else
+            return System.Threading.Tasks.Task.Run(function);
+#endif
+
+
+
+        }
+
+
+        public static System.Threading.Tasks.Task<TResult> Run<TResult>(System.Func<System.Threading.Tasks.Task<TResult>> function
+            , System.Threading.CancellationToken cancellationToken)
+        {
+
+
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(function, cancellationToken);
+#else
+            return System.Threading.Tasks.Task.Run(function, cancellationToken);
+#endif
+
+
+        }
+
+
+        public static System.Threading.Tasks.Task<TResult> Run<TResult>(System.Func<TResult> function)
+        {
+
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(function);
+#else
+            return System.Threading.Tasks.Task.Run(function);
+#endif
+
+
+        }
+
+
+        public static System.Threading.Tasks.Task<TResult> Run<TResult>(System.Func<TResult> function
+            , System.Threading.CancellationToken cancellationToken)
+        {
+
+#if DOTNET4
+            return System.Threading.Tasks.TaskEx.Run(function, cancellationToken);
+#else
+            return System.Threading.Tasks.Task.Run(function, cancellationToken);
+#endif
+
+        }
+
+
+    }
+
+
+}
---
+++ source/MasterDev.ChromeDevTools.DotNet4/Net4/TypeExtensions.cs
@@ -0,0 +1,39 @@
+﻿
+namespace System
+{
+
+
+
+    public static class TypeExtensions
+    {
+
+
+        public static System.Type GetTypeInfo(this System.Type t)
+        {
+            return t;
+        }
+
+
+        public static object[] GetCustomAttributes(this System.Type t, System.Type tt)
+        {
+            return t.GetCustomAttributes(tt, true);
+        }
+
+
+        // https://github.com/dotnet/corefx/blob/master/src/Common/src/CoreLib/System/Type.cs#L61
+        // public virtual Type[] GenericTypeArguments => (IsGenericType && !IsGenericTypeDefinition) ? GetGenericArguments() : Array.Empty<Type>();
+        public static System.Type[] GetGenericTypeArguments(this System.Type t)
+        {
+            // return t.GenericTypeArguments
+            if (t.IsGenericType && !t.IsGenericTypeDefinition)
+                return t.GetGenericArguments();
+
+            return new System.Type[0];
+        }
+
+
+        //public static Task Run();
+    }
+
+
+}
